2839da1a11c72c95f30c7e57d78ce26345a49de5
cthor <cdthorpe211@gmail.com>
2024-09-30 18:27:35 -0600

    changes message from varchar to text

--- server/db/sql/schema.sql
+++ server/db/sql/schema.sql
@@ -1,25 +1,31 @@
-CREATE TABLE IF NOT EXISTS users(
-    user_id VARCHAR(36) PRIMARY KEY,
-    password varchar(256) NOT NULL,
-    username varchar(256) NOT NULL
+USE CACOPHONY_DB;
+
+CREATE TABLE IF NOT EXISTS users (
+     user_id VARCHAR(36) PRIMARY KEY,
+     username VARCHAR(256) NOT NULL,
+     password VARCHAR(256) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS conversation(
+CREATE TABLE IF NOT EXISTS conversation (
     conversation_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    conversation_name varchar(256)
+    conversation_name VARCHAR(256)
 );
 
-CREATE TABLE IF NOT EXISTS user_subscriptions(
+CREATE TABLE IF NOT EXISTS user_subscriptions (
     user_subscriptions_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    FOREIGN KEY user_id VARCHAR(36) REFERENCES users (user_id),
-    FOREIGN KEY conversation_id INT UNSIGNED REFERENCES conversation (conversation_id)
+    user_id VARCHAR(36),
+    conversation_id INT UNSIGNED,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (conversation_id) REFERENCES conversation(conversation_id)
 );
 
-CREATE TABLE IF NOT EXISTS messages(
+CREATE TABLE IF NOT EXISTS messages (
     message_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     message_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    message VARCHAR(32768) NOT NULL,
-    FOREIGN KEY user_id VARCHAR(36) NOT NULL REFERENCES users (user_id),
-    FOREIGN KEY conversation_id INT UNSIGNED NOT NULL REFERENCES conversation (conversation_id)
-    edited BOOL DEFAULT FALSE
+    message TEXT NOT NULL,
+    edited BOOL DEFAULT FALSE,
+    user_id VARCHAR(36) NOT NULL,
+    conversation_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (conversation_id) REFERENCES conversation(conversation_id)
 );

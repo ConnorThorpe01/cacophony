bc254aef649a89d7133766096c32938be1f4c6f4
cthor <cdthorpe211@gmail.com>
2024-10-09 16:55:24 -0600

    set up redis container

--- server/go.mod
+++ server/go.mod
@@ -7,6 +7,7 @@ require google.golang.org/grpc v1.66.2
 require (
 	cacophony v0.0.0
 	github.com/DATA-DOG/go-sqlmock v1.5.2
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/golang-jwt/jwt/v4 v4.5.0
 	github.com/google/uuid v1.6.0
@@ -16,7 +17,9 @@ require (
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
--- server/main.go
+++ server/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	cacophony "cacophony/proto"
+	"context"
 	"database/sql"
 	"flag"
 	"fmt"
+	"github.com/go-redis/redis/v8"
 	_ "github.com/go-sql-driver/mysql"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -18,34 +20,51 @@ var (
 )
 
 func main() {
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
+	sqlDBHost := os.Getenv("DB_HOST")
+	sqlDBPort := os.Getenv("DB_PORT")
+	sqlDBUser := os.Getenv("DB_USER")
+	sqlDBPassword := os.Getenv("DB_PASSWORD")
+	sqlDBName := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", sqlDBUser, sqlDBPassword, sqlDBHost, sqlDBPort, sqlDBName)
 	fmt.Println(dsn)
-	db, err := sql.Open("mysql", dsn)
+	sqlDB, err := sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatalf("Failed to connect to the database: %v", err)
 	}
-	defer db.Close()
+	defer sqlDB.Close()
 
 	// Configure connection pooling settings
-	db.SetMaxOpenConns(5)    // Maximum number of open connections
-	db.SetMaxIdleConns(3)    // Maximum number of idle connections
-	db.SetConnMaxLifetime(0) // Max connection lifetime (0 means unlimited)
-	if err = db.Ping(); err != nil {
+	sqlDB.SetMaxOpenConns(5)    // Maximum number of open connections
+	sqlDB.SetMaxIdleConns(3)    // Maximum number of idle connections
+	sqlDB.SetConnMaxLifetime(0) // Max connection lifetime (0 means unlimited)
+	if err = sqlDB.Ping(); err != nil {
 		log.Fatalf("Failed to ping to the database: %v", err)
 	}
+
+	ctx := context.Background()
+	rHost := os.Getenv("REDIS_HOST")
+	rPort := os.Getenv("REDIS_PORT")
+	rPass := os.Getenv("REDIS_PASSWORD")
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", rHost, rPort),
+		Password: rPass, // Redis password here
+		DB:       0,     // Use default DB
+	})
+
+	err = rdb.Ping(ctx).Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	flag.Parse()
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	cacophony.RegisterChatServiceServer(s, &server{db: db})
+	cacophony.RegisterChatServiceServer(s, &server{db: sqlDB})
 	log.Printf("server listening at %v", lis.Addr())
 
 	reflection.Register(s)
